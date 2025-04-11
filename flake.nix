34f61090a8614e00ffb0b20e7d91ef0000b9a391
Ivan Sadílek <sadilekivan@gmail.com>
2025-04-11 17:37:39 +0200

    adding nix dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python312
+            python312Packages.human-readable
+            python312Packages.typer
+          ];
+        };
+      }
+    );
+}
